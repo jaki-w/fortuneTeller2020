8d9e4f4e1fe43f2b49124529f0dc83dbc4773aac
Jaki Widby <jaki.widby@gmail.com>
2020-08-18 12:12:08 -0700

    create function to determine luck based on value of checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
+function determineLuck(value) {
+  let luck = "";
+  if (value <= 5) {
+    luck = "unlucky";
+  } else if (value <= 7) {
+    luck = "neutral";
+  } else {
+    luck = "lucky"
+  }
+  return luck;
+}
+
+// function determineFortune(fortune)
+ 
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     $(".showLater").show();
+    let valueCounter = 0;
     $("input:checkbox[name=occurances]:checked").each(function() {
-      $("#returnResults").append($(this).val());
+      const occurance = parseInt($(this).val());
+      valueCounter += occurance;
     });
+    const luck = determineLuck(valueCounter);
+    console.log(luck);
+    console.log(valueCounter);
   });
 });
